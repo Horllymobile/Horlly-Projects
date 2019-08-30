0ff3b80f7c81b30a209797d7b332caa45fa7f43e
Horllymobile <45019164+Horllymobile@users.noreply.github.com>
2019-08-29 21:34:57 -0700

    Update calculator.py
    it's just a simple calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,21 @@
 
+number1 = float(input("Enter first number: "))
+operator = input("Enter operator")
+number2 = float(input("Enter second number: "))
+
+
+if operator == '+':
+  result = number1 + number2
+  print(operator)
+elif operator == '-':
+  result = number1 - number2
+  print(operator)
+elif operator == '/':
+  result = number1 / number2
+  print(operator)
+elif operator == '%':
+  result = number1 % number2
+  print(operator)
+elif operator == '*':
+  result = number1 * number2
+  print(operator)
